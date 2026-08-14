94cfe333afe2cb479c8056ce0a9949872cf60ae7
Yashvee Sharma <yashvee2008@gmail.com>
2026-08-14 12:00:00 +0530

    Add program8.c on 2026-08-14

---
+++ program8.c
@@ -0,0 +1,27 @@
+// Day 4: Program 2
+// Q8: Write a program to find and display the sum of the first n natural numbers.
+
+// /*
+// Sample Test Cases:
+// Input 1:
+// 5
+// Output 1:
+// Sum=15
+
+// Input 2:
+// 10
+// Output 2:
+// Sum=55
+
+// */
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    int sum = 0;
+    for (int i = 0; i <= n; i++){
+        sum += i;
+    }
+    printf("The sum of first %d natural numbers is: %d\n", n, sum);
+}
