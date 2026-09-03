3a69f7e28ad157aa9fd18e0467b4710f5187d6f1
Yashvee Sharma <yashvee2008@gmail.com>
2026-09-03 12:00:00 +0530

    Add program49.c on 2026-09-03

---
+++ program49.c
@@ -0,0 +1,33 @@
+// Q49: Write a program to print the following pattern:
+// 5
+// 45
+// 345
+// 2345
+// 12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+5
+45
+345
+2345
+12345
+
+*/
+
+#include <stdio.h>
+int main(){
+    int i, j, n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    for (i = n; i >= 1; i--){
+        for (j=i; j >= 1; j--){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
