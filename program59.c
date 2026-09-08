5863b12ca26c494e970135dd69ac31783ce48049
Yashvee Sharma <yashvee2008@gmail.com>
2026-09-08 12:00:00 +0530

    Add program59.c on 2026-09-08

---
+++ program59.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, evenCount = 0, oddCount = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        
+        if(arr[i] % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+
+    printf("Total even numbers = %d\n", evenCount);
+    printf("Total odd numbers = %d\n", oddCount);
+    return 0;
+}
